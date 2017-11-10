8a55f83e220b789262509a798468632f238d454a
magiclobster <32847577+magiclobster@users.noreply.github.com>
2017-11-10 22:05:18 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-BACKUP_PATH="~/crypto-pool-data/"
+BACKUP_PATH="/tmp/backups/crypto-pool"
 
-mkdir -p ${BACKUP_PATH}{redis,wallet}
+mkdir -p ${BACKUP_PATH}/{redis,wallet}
 
+LAST_REDIS_SAVE_TIME=$(docker exec -it aeon-redis redis-cli lastsave | awk '{print $2}' | tr -d '\r')
+docker exec -it aeon-redis redis-cli bgsave
+REDIS_SAVE_TIME=$(docker exec -it aeon-redis redis-cli lastsave | awk '{print $2}' | tr -d '\r')
+sleep 3
+if [ ${REDIS_SAVE_TIME} -lt ${LAST_REDIS_SAVE_TIME} ]; then
+    echo "WARNING: redis dump may be out of date!"
+fi
 docker cp aeon-redis:/data/dump.rdb ${BACKUP_PATH}/redis
-docker exec -it aeon-wallet ./get_seed > ${BACKUP_PATH}/wallet
+docker exec -it aeon-wallet get_seed > ${BACKUP_PATH}/wallet/aeon-wallet-seed
