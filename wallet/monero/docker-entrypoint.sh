139a486dec0b8d67c81bbaabe53a83ace0dd6b30
magiclobster <magiclobster@mailbox.org>
2017-11-11 12:40:40 +0100

    wallet fix

--- wallet/monero/docker-entrypoint.sh
+++ wallet/monero/docker-entrypoint.sh
@@ -12,7 +12,6 @@ if [ "${FORCE_BLOCKCHAIN_DOWNLOAD}" == "true" ]; then
     monero-blockchain-import --verify 0 --input-file /tmp/blockchain.raw
     rm /tmp/blockchain.raw
 fi
-
 monerod --data-dir /wallet/data --rpc-bind-ip 0.0.0.0 --rpc-bind-port ${WALLETD_PORT} --log-file /wallet/walletd.log --confirm-external-bind --non-interactive --detach
 sleep 10
-exec monero-wallet-cli --wallet-file /wallet/${WALLET_NAME} --password ${WALLET_PASSWORD} --daemon-address 127.0.0.1:${WALLETD_PORT}
+exec monero-wallet-rpc --wallet-file /wallet/${WALLET_NAME} --password ${WALLET_PASSWORD} --daemon-address 127.0.0.1:${WALLETD_PORT} --rpc-bind-ip 0.0.0.0 --rpc-bind-port ${WALLET_PORT} --confirm-external-bind
