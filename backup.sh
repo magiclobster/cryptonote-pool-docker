dca5f61d310297eaefbf98fa5aa1f0bddc5e22e7
magiclobster <magiclobster@mailbox.org>
2017-11-11 14:49:35 +0100

    confirueation changes for monero integration

--- backup.sh
+++ backup.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
 BACKUP_PATH="/tmp/backups/crypto-pool"
+REDIS_CONTAINER_NAME="cryptonote-redis"
+AEON_WALLET_CONTAINER="aeon-wallet"
+MONERO_WALLET_CONTAINER="monero-wallet"
 
 mkdir -p ${BACKUP_PATH}/{redis,wallet}
 
-LAST_REDIS_SAVE_TIME=$(docker exec -it aeon-redis redis-cli lastsave | awk '{print $2}' | tr -d '\r')
-docker exec -it aeon-redis redis-cli bgsave
-REDIS_SAVE_TIME=$(docker exec -it aeon-redis redis-cli lastsave | awk '{print $2}' | tr -d '\r')
+LAST_REDIS_SAVE_TIME=$(docker exec -it ${REDIS_CONTAINER_NAME} redis-cli lastsave | awk '{print $2}' | tr -d '\r')
+docker exec -it ${REDIS_CONTAINER_NAME} redis-cli bgsave
+REDIS_SAVE_TIME=$(docker exec -it ${REDIS_CONTAINER_NAME} redis-cli lastsave | awk '{print $2}' | tr -d '\r')
 sleep 3
 if [ ${REDIS_SAVE_TIME} -lt ${LAST_REDIS_SAVE_TIME} ]; then
     echo "WARNING: redis dump may be out of date!"
 fi
-docker cp aeon-redis:/data/dump.rdb ${BACKUP_PATH}/redis
-docker exec -it aeon-wallet get_seed | grep key | cut -d ":" -f 2 > ${BACKUP_PATH}/wallet/aeon-wallet-seed
+docker cp ${REDIS_CONTAINER_NAME}:/data/dump.rdb ${BACKUP_PATH}/redis
+docker exec -it ${AEON_WALLET_CONTAINER} get_seed | grep key | cut -d ":" -f 2 > ${BACKUP_PATH}/wallet/${AEON_WALLET_CONTAINER}-seed
+docker exec -it ${MONERO_WALLET_CONTAINER} get_seed | grep key | cut -d ":" -f 2 > ${BACKUP_PATH}/wallet/${MONERO_WALLET_CONTAINER}-seed
--- haproxy/docker-entrypoint.sh
+++ haproxy/docker-entrypoint.sh
@@ -5,13 +5,19 @@ if [ ! -f /etc/letsencrypt/haproxy/${AEON_WEB_DOMAIN}.pem ]; then
     create_ss ${AEON_WEB_DOMAIN}
 fi
 
-sed -i "s/°HAPROXY_STATS_PASSWORD°/${HAPROXY_STATS_PASSWORD}/g" /usr/local/etc/haproxy/haproxy.cfg
+if [ ! -f /etc/letsencrypt/haproxy/${MOONER_WEB_DOMAIN}.pem ]; then
+    echo "ssl cert missing, creating self signed"
+    create_ss ${MOONERO_WEB_DOMAIN}
+fi
+
 sed -i "s/°AEON_WEB_DOMAIN°/${AEON_WEB_DOMAIN}/g" /usr/local/etc/haproxy/haproxy.cfg
-sed -i "s/°AEON_PORT_LOW°/${AEON_PORT_LOW}/g" /usr/local/etc/haproxy/haproxy.cfg
-sed -i "s/°AEON_PORT_MED°/${AEON_PORT_MED}/g" /usr/local/etc/haproxy/haproxy.cfg
-sed -i "s/°AEON_PORT_HIG°/${AEON_PORT_HIG}/g" /usr/local/etc/haproxy/haproxy.cfg
-sed -i "s/°AEON_PORT_EXT°/${AEON_PORT_EXT}/g" /usr/local/etc/haproxy/haproxy.cfg
-sed -i "s/°AEON_API_PORT°/${AEON_API_PORT}/g" /usr/local/etc/haproxy/haproxy.cfg
+sed -i "s/°AEON_WEB_DOMAIN°/${MONERO_WEB_DOMAIN}/g" /usr/local/etc/haproxy/haproxy.cfg
+
+sed -i "s/°HAPROXY_STATS_PASSWORD°/${HAPROXY_STATS_PASSWORD}/g" /usr/local/etc/haproxy/haproxy.cfg
+sed -i "s/°MINER_PORT_LOW°/${MINER_PORT_LOW}/g" /usr/local/etc/haproxy/haproxy.cfg
+sed -i "s/°MINER_PORT_MED°/${MINER_PORT_MED}/g" /usr/local/etc/haproxy/haproxy.cfg
+sed -i "s/°MINER_PORT_HIG°/${MINER_PORT_HIG}/g" /usr/local/etc/haproxy/haproxy.cfg
+sed -i "s/°POOL_API_PORT°/${POOL_API_PORT}/g" /usr/local/etc/haproxy/haproxy.cfg
 
 service cron start
 exec /docker-entrypoint.sh "$@"
