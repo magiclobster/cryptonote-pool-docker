2f6aa99880532f6b6bd4aee7127206fef82bfae8
magiclobster <magiclobster@mailbox.org>
2017-11-11 04:46:55 +0100

    static linking monero and aeon

--- wallet/monero/docker-entrypoint.sh
+++ wallet/monero/docker-entrypoint.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-sleep infinity
-
 if [ ! -f /wallet/${WALLET_NAME} ]; then
     echo "wallet.sh not found, creating new wallet.sh"
-    echo "exit" | /wallet/wallet --generate-new-wallet ${WALLET_NAME} --password ${WALLET_PASSWORD} > /dev/null &
+    echo "1" | monero-wallet-cli --generate-new-wallet ${WALLET_NAME} --password ${WALLET_PASSWORD} > /dev/null &
     sleep 5
 fi
 
-./walletd --data-dir data --rpc-bind-ip 0.0.0.0 --rpc-bind-port ${WALLETD_PORT} --log-file /wallet/walletd.log --no-console --no-igd &> /dev/null &
-sleep 10
-exec /wallet/wallet --wallet-file /wallet/${WALLET_NAME} --password ${WALLET_PASSWORD} --rpc-bind-ip 0.0.0.0 --rpc-bind-port ${WALLET_PORT} --daemon-address 127.0.0.1:${WALLETD_PORT}
+if [ "${FORCE_BLOCKCHAIN_DOWNLOAD}" == "true" ]; then
+    echo "force blockchain download == true, downloading ..."
+    curl --insecure -s -o /tmp/blockchain.raw https://downloads.getmonero.org/blockchain.raw
+    monero-blockchain-import --verify 0 --input-file /tmp/blockchain.raw
+    rm /tmp/blockchain.raw
+fi
 
+monerod --data-dir /wallet/data --rpc-bind-ip 0.0.0.0 --rpc-bind-port ${WALLETD_PORT} --log-file /wallet/walletd.log --confirm-external-bind --non-interactive --detach
+sleep 10
+exec monero-wallet-cli --wallet-file /wallet/${WALLET_NAME} --password ${WALLET_PASSWORD} --daemon-address 127.0.0.1:${WALLETD_PORT}
