6f3197a6f5de2666462312a85c901db73eb4d278
magiclobster <32847577+magiclobster@users.noreply.github.com>
2017-11-15 14:46:09 +0100

    Update recreate_certs.sh

--- haproxy/cert_scripts/recreate_certs.sh
+++ haproxy/cert_scripts/recreate_certs.sh
@@ -4,5 +4,9 @@ LE_CERT_DIR=/etc/letsencrypt/live/
 HA_CERT_DIR=/etc/letsencrypt/haproxy/
 
 for d in ${LE_CERT_DIR}*/; do
-    cat ${d}fullchain*.pem ${d}privkey*.pem > ${HA_CERT_DIR}$(basename ${d}).pem
+    if [ -f ${d}fullchain*.pem ] && [ ${d}privkey*.pem ]; then
+        cat ${d}fullchain*.pem ${d}privkey*.pem > ${HA_CERT_DIR}$(basename ${d}).pem
+    else
+        echo "missing or wrong name for cert/private key in: ${d}"
+    fi
 done
